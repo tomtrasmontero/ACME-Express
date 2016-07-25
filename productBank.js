a25fdc6b0ed1bd2996597ec23547edb4b3ca1c31
Tom Trasmontero <tomtrasmontero@Toms-MBP.home>
2016-07-25 02:51:01 -0400

    update all functionality except edit

---
+++ productBank.js
@@ -0,0 +1,48 @@
+var products = [
+	{
+		id: 1,
+		name: 'Shoe'
+	},
+	{
+		id:2,
+		name: 'Bike'
+	}
+];
+
+
+function addID(){
+	var idNum = products.length;
+	idNum++;
+	return idNum;
+} 
+
+function editName(id,name){
+	for( var i = 0; i < products.length; i++){
+		if(products[i].id === id){
+			products[i].name = name;
+		}
+	}		
+}
+
+module.exports = {
+	getProducts: function(){
+		return products;
+	},
+	deleteProduct: function(id){
+		var toDelete = this.getProducts().filter(function(item){
+			return item.id === id;
+		})[0];
+		var idx = this.getProducts().indexOf(toDelete);
+		this.getProducts().splice(idx, 1);
+
+	},
+	addProduct: function(item){
+		var newID = addID();
+		this.getProducts().push({id: newID, name: item});
+
+	},
+	editProduct: function(id, name){
+		editName(id,name);		
+	}
+};
+
